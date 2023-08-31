5e1dd8a3250e813bc5d96ea4fffbc4a4d2ebf77c
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-08-31 02:07:31 -0300

    feat: Finalizando projeto para entrega

--- src/app/components/card/card-price/card-price.component.ts
+++ src/app/components/card/card-price/card-price.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card-price',
-  templateUrl: './card-price.component.html',
-  styleUrls: ['./card-price.component.css']
+	selector: 'app-card-price',
+	templateUrl: './card-price.component.html',
+	styleUrls: ['./card-price.component.css'],
 })
 export class CardPriceComponent {
-
+	@Input() price: number = 0.0;
+	@Input() gameType: string = '';
 }
---
+++ src/app/pages/home/db.ts
@@ -0,0 +1,30 @@
+export const database = [
+	{
+		id: 1,
+		pic: '../../../assets/ac-cover.jpg',
+		label: 'Exclusive',
+		plataform: 'PC - PS4 - XBOX ONE',
+		price: 59.99,
+	},
+	{
+		id: 2,
+		pic: '../../../assets/bt-4.jpg',
+		label: 'Remastered',
+		plataform: 'XBOX ONE',
+		price: 89.99,
+	},
+	{
+		id: 3,
+		pic: '../../../assets/bt-5.jpg',
+		label: '50% OFF',
+		plataform: 'PC - PS4',
+		price: 79.99,
+	},
+	{
+		id: 4,
+		pic: '../../../assets/bt-bad-company-2.jpg',
+		label: 'Exclusive',
+		plataform: 'PC',
+		price: 29.9,
+	},
+];
